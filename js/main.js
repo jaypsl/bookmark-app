82b97796f53784e370904809cf106f4d620feaab
Jaypal Solanki <jaypsl263@gmail.com>
2018-06-27 02:23:09 -0400

    fetching bookmarks

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // Save Bookmark
 function saveBookmark(e){
-  // Get form values
+  
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
 
@@ -16,44 +16,57 @@ function saveBookmark(e){
     url: siteUrl
   }
 
-  /*
-    // Local Storage Test
-    localStorage.setItem('test', 'Hello World');
-    console.log(localStorage.getItem('test'));
-    localStorage.removeItem('test');
-    console.log(localStorage.getItem('test'));
-  */
-
   // Test if bookmarks is null
   if(localStorage.getItem('bookmarks') === null){
-    // Init array
+   
     var bookmarks = [];
-    // Add to array
+    
     bookmarks.push(bookmark);
-    // Set to localStorage
+    
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   } else {
-    // Get bookmarks from localStorage
+   
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    // Add bookmark to array
+  
     bookmarks.push(bookmark);
-    // Re-set back to localStorage
+    
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
 
-  // Clear form
+
   document.getElementById('myForm').reset();
 
-  // Re-fetch bookmarks
-  
+ 
+  fetchBookmarks()
 
-  // Prevent form from submitting
+ 
   e.preventDefault();
 }
 
 
-}
 
+
+// Fetch bookmarks
+function fetchBookmarks(){
+ 
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+ 
+  var bookmarksResults = document.getElementById('bookmarksResults');
+
+
+  bookmarksResults.innerHTML = '';
+  for(var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += '<div class="well">'+
+                                  '<h3>'+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                  '</h3>'+
+                                  '</div>';
+  }
+}
 
 // Validate Form
 function validateForm(siteName, siteUrl){
