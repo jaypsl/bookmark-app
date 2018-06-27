636a94f080c78de5ae103367137840aaf4b6dee5
Jaypal Solanki <jaypsl263@gmail.com>
2018-06-27 02:14:32 -0400

    using local storage to save data

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+// Save Bookmark
+function saveBookmark(e){
+  // Get form values
+  var siteName = document.getElementById('siteName').value;
+  var siteUrl = document.getElementById('siteUrl').value;
+
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
+
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+  /*
+    // Local Storage Test
+    localStorage.setItem('test', 'Hello World');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+  */
+
+  // Test if bookmarks is null
+  if(localStorage.getItem('bookmarks') === null){
+    // Init array
+    var bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Add bookmark to array
+    bookmarks.push(bookmark);
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
+  // Clear form
+  document.getElementById('myForm').reset();
+
+  // Re-fetch bookmarks
+  
+
+  // Prevent form from submitting
+  e.preventDefault();
+}
+
+
+}
+
+
+// Validate Form
+function validateForm(siteName, siteUrl){
+  if(!siteName || !siteUrl){
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
+}
